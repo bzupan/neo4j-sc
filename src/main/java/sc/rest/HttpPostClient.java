da6554b70bc28f6dc92ce1a9b391b2a42e184f71
brane <brane@bzupan.smart-com.si>
2018-12-07 15:26:54 +0100

    cleanup

--- src/main/java/sc/rest/HttpPostClient.java
+++ src/main/java/sc/rest/HttpPostClient.java
@@ -1,31 +1,23 @@
 package sc.rest;
 
-import java.util.List;
-
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
-
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Node;
 
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpEntity;
@@ -45,122 +37,174 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLContextBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.neo4j.graphdb.Relationship;
 
 /**
- * This is an Neo4j node properties post .
+ * post from Neo4j
  */
 public class HttpPostClient {
 
-    private static final HttpPostJson http = new HttpPostJson("insecure");
+    private static final HttpPostJson http = new HttpPostJson();
+    private static final String httpClientProperties = "{enableInsecureHttps: false, enableErrorMessage:false}";
 
     @Context
     public Log log;
 
     @UserFunction
-    @Description("sc.rest.httpPostJson(\"url string\", node) - return json post Response")
+    @Description(
+            "// - return json post response\n"
+            + "RETURN sc.rest.httpPostJson(\"https://127.0.0.1:8443/restTest\", {jsonObject:123},{enableInsecureHttps: false, enableErrorMessage:false}) AS postResponse "
+    )
     public Map<String, Object> httpPostJson(
             @Name("url") String url,
-            @Name("node") Object inputObject
+            @Name("node") Object inputObject,
+            @Name(value = "httpClientOptions", defaultValue = httpClientProperties) Map<String, Object> httpClientOptions
     ) {
-        Map<String, Object> reaponse = http.post(url, inputObject);
+        log.debug("sc.rest.httpPostJson - input: " + url.toString() + " " + inputObject.toString() + " " + httpClientOptions.toString());
+        Map<String, Object> reaponse = http.post(url, inputObject, httpClientOptions);
+        log.debug("sc.rest.httpPostJson - reaponse: " + url.toString() + " " + inputObject.toString() + " " + httpClientOptions.toString());
         return reaponse;
     }
 
     @UserFunction
-    @Description("sc.rest.httpPostNode(\"url string\", node) - return json post")
+    @Description(
+            "// - return node post response\n"
+            + "MATCH (n:scTestNode) WHERE ID(n)=434\n"
+            + "RETURN sc.rest.httpPostNode(\n"
+            + "\"http://127.0.0.1/restTest\", \n"
+            + "n,\n"
+            + "{enableInsecureHttps: false, enableErrorMessage:false}\n"
+            + ") \n"
+            + "AS postResponseNode"
+    )
     public Map<String, Object> httpPostNode(
             @Name("url") String url,
-            @Name("node") Node node
+            @Name("node") Node node,
+            @Name(value = "httpClientOptions", defaultValue = httpClientProperties) Map<String, Object> httpClientOptions
     ) {
-        // Map<String, Object> cypherNodeTmp = new HashMap<String, Object>(node.getAllProperties());
-        // cypherNodeTmp.put("_id", node.getId());
-        // cypherNodeTmp.put("_labels", node.getLabels());
-        Map<String, Object> reaponse = http.post(url, node.getAllProperties());
-        return reaponse;//content.toString();
+        log.debug("sc.rest.httpPostNode - input: " + url.toString() + " " + node.toString() + " " + httpClientOptions.toString());
+        Map<String, Object> reaponse = http.post(url, node.getAllProperties(), httpClientOptions);
+        log.debug("sc.rest.httpPostNode - reaponse: " + url.toString() + " " + node.toString() + " " + httpClientOptions.toString());
+        return reaponse;
     }
 
     @UserFunction
-    @Description("sc.rest.httpPostRelationship(\"url string\", Relationship) - return json post")
+    @Description("\n" +
+"            \"// - return node post response\\n\"\n" +
+"            + \"MATCH ()-[l]->()HERE ID(l)=434\\n\"\n" +
+"            + \"RETURN sc.rest.httpPostNode(\\n\"\n" +
+"            + \"\\\"http://127.0.0.1/restTest\\\", \\n\"\n" +
+"            + \"n,\\n\"\n" +
+"            + \"{enableInsecureHttps: false, enableErrorMessage:false}\\n\"\n" +
+"            + \") \\n\"\n" +
+"            + \"AS postResponseNode\"\n" +
+"    ")
     public Map<String, Object> httpPostRelationship(
             @Name("url") String url,
-            @Name("relationship") Relationship relationship
+            @Name("relationship") Relationship relationship,
+            @Name(value = "httpClientOptions", defaultValue = httpClientProperties) Map<String, Object> httpClientOptions
     ) {
-        // Map<String, Object> cypherNodeTmp = new HashMap<String, Object>(node.getAllProperties());
-        // cypherNodeTmp.put("_id", node.getId());
-        // cypherNodeTmp.put("_labels", node.getLabels());
-        Map<String, Object> reaponse = http.post(url, relationship.getAllProperties());
-        return reaponse;//content.toString();
+        log.debug("sc.rest.httpPostRelationship - input: " + url.toString() + " " + relationship.toString() + " " + httpClientOptions.toString());
+        Map<String, Object> reaponse = http.post(url, relationship.getAllProperties(), httpClientOptions);
+        log.debug("sc.rest.httpPostRelationship - reaponse: " + url.toString() + " " + relationship.toString() + " " + httpClientOptions.toString());
+        return reaponse;
     }
 
     // --- http https post
     public static class HttpPostJson {
 
-        CloseableHttpClient httpClient;
+        CloseableHttpClient httpClientSecure;
+        CloseableHttpClient httpClientInsecure;
 
-        public HttpPostJson(String httpType) {
-            if (httpType.equals("insecure")) {
-                try {
-                    final SSLContext sslContext = new SSLContextBuilder()
-                            .loadTrustMaterial(null, (x509CertChain, authType) -> true)
-                            .build();
-                    httpClient = HttpClientBuilder.create()
-                            .setSSLContext(sslContext)
-                            .setConnectionManager(
-                                    new PoolingHttpClientConnectionManager(
-                                            RegistryBuilder.<ConnectionSocketFactory>create()
-                                                    .register("http", PlainConnectionSocketFactory.INSTANCE)
-                                                    .register("https", new SSLConnectionSocketFactory(sslContext,
-                                                            NoopHostnameVerifier.INSTANCE))
-                                                    .build()
-                                    ))
-                            .build();
-                } catch (Exception ex) {
-                    System.out.println(ex.toString());
-                    httpClient = HttpClientBuilder.create().build();
-                }
-            } else {
-                httpClient = HttpClientBuilder.create().build();
+        public HttpPostJson() {
+            try {
+                // --- httpClient insecure
+                final SSLContext sslContext = new SSLContextBuilder()
+                        .loadTrustMaterial(null, (x509CertChain, authType) -> true)
+                        .build();
+                httpClientInsecure = HttpClientBuilder.create()
+                        .setSSLContext(sslContext)
+                        .setConnectionManager(
+                                new PoolingHttpClientConnectionManager(
+                                        RegistryBuilder.<ConnectionSocketFactory>create()
+                                                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                                                .register("https", new SSLConnectionSocketFactory(sslContext,
+                                                        NoopHostnameVerifier.INSTANCE))
+                                                .build()
+                                ))
+                        .build();
+            } catch (Exception ex) {
+                System.out.println("sc.rest.httpPostRelationship - error: " + ex.toString());
             }
+
+            // --- httpClient secure
+            httpClientSecure = HttpClientBuilder.create().build();
         }
 
-        public Map<String, Object> post(String url, Object inputObject) {
-            Map<String, Object> map = new HashMap<String, Object>();
+        public Map<String, Object> post(String url, Object inputObject, Map<String, Object> httpClientOptions) {
+
+            CloseableHttpClient httpClient;
+
+            // -- check for client type
+            if (httpClientOptions.get("enableInsecureHttps").equals(false)) {
+                httpClient = httpClientSecure;
+            } else {
+                httpClient = httpClientInsecure;
+            }
+
+            // --- result variables
+            Map<String, Object> httpClientResultMap = new HashMap<String, Object>();
+            Boolean httpClientError = false;
+
+            // --- http post
             try {
-                //Object to JSON in String
+                // --- Object to JSON 
                 ObjectMapper mapper = new ObjectMapper();
-                Gson gson = new Gson();
-
-                //   log.info("httpJsonPost source: " + node.getId() + " " + node.getLabels().toString() + " " + node.getAllProperties().toString());
                 String jsonInString = mapper.writeValueAsString(inputObject);
-                System.out.println("httpJsonPost as json: " + jsonInString.toString());
 
+                // --- post
                 HttpPost postRequest = new HttpPost(url);
-
                 postRequest.setEntity(new StringEntity(jsonInString));
                 postRequest.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-
                 CloseableHttpResponse httpResponse = httpClient.execute(postRequest);
+
+                // --- response
                 String content = EntityUtils.toString(httpResponse.getEntity());
                 int statusCode = httpResponse.getStatusLine().getStatusCode();
-                System.out.println("httpJsonPost statusCode = " + statusCode + " content = " + content);
 
-                map = new Gson().fromJson(content.toString(), Map.class);
+                httpClientResultMap = new Gson().fromJson(content.toString(), Map.class);
+                httpClientError = false;
 
             } catch (JsonProcessingException ex) {
-                System.out.println(ex.toString());
-                map.put("error", "JsonProcessingException");
+                System.out.println("sc.rest.httpPostRelationship - error: JsonProcessingException " + ex.toString());
+                httpClientResultMap.put("error", "JsonProcessingException");
+                httpClientError = true;
             } catch (UnsupportedEncodingException ex) {
-                System.out.println(ex.toString());
-                map.put("error", "UnsupportedEncodingException");
+                System.out.println("sc.rest.httpPostRelationship - error: UnsupportedEncodingException " + ex.toString());
+                httpClientResultMap.put("error", "UnsupportedEncodingException");
+                httpClientError = true;
             } catch (IOException ex) {
-                System.out.println(ex.toString());
-                map.put("error", "IOException");
+                System.out.println("sc.rest.httpPostRelationship - error: IOException" + ex.toString());
+                httpClientResultMap.put("error", "IOException");
+                httpClientError = true;
             } catch (Exception ex) {
-                System.out.println(ex.toString());
-                map.put("error", "Exception");
+                System.out.println("sc.rest.httpPostRelationship - error: " + ex.toString());
+                httpClientResultMap.put("error", "Exception");
+                httpClientError = true;
             }
-            return map;
+
+            if (httpClientOptions.get("enableErrorMessage").equals(true)) {
+                // --- send result or error message 
+                return httpClientResultMap;
+            } else {
+                // --- send null on error 
+                if (httpClientError.equals(true)) {
+                    // --- send null on error
+                    return null;
+                } else {
+                    // --- return result
+                    return httpClientResultMap;
+                }
+            }
         }
     }
 }
--- src/main/java/sc/rest/UrlEncodeString.java
+++ src/main/java/sc/rest/UrlEncodeString.java
@@ -1,7 +1,5 @@
 package sc.rest;
 
-import java.util.List;
-
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
@@ -10,11 +8,10 @@ import org.neo4j.procedure.Context;
 
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLEncoder;
 
 /**
- * url encode string.
+ * url encode string
  */
 public class UrlEncodeString {
 
@@ -22,17 +19,14 @@ public class UrlEncodeString {
     public Log log;
 
     @UserFunction
-    @Description("RETURN sc.urlEncodeString('{\"jsonrpc\":\"2.0\",\"method\":\"pingIpAddress\",\"params\":{\"ipAddress\":\"10.10.103.254\",\"pingCount\":2,\"pingInterval\":1,\"pingPacketSize\":56,\"timeoutSec\":20},\"id\":null}') - return  url encoded string")
+    @Description("RETURN sc.rest.urlEncodeString('aaa.bbb@email.com {jsonString:\"!\"#$&/()\"}') AS urlEncodeString) // return  url encoded string")
     public String urlEncodeString(
-            @Name("strings") String stringUrl
+            @Name("stringUrl") String stringUrl
     ) throws MalformedURLException,
             UnsupportedEncodingException {
-        log.debug("sc.urlEncodeString source: " + stringUrl);
-
+        log.debug("sc.rest.urlEncodeString - input: " + stringUrl);
         String stringUrlEncoded = URLEncoder.encode(stringUrl, "UTF-8");
-
-        log.debug("sc.urlEncodeString encoded: " + stringUrlEncoded);
-
+        log.debug("sc.rest.urlEncodeString - encoded: " + stringUrlEncoded);
         return stringUrlEncoded;
     }
 }
